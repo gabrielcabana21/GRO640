cfd6807fe56fd2707f2c06b234b2c5a592035b54
Alexandre Girard <alx87grd@gmail.com>
2021-05-02 15:12:23 -0400

    moved test

--- projects/value_iteration/double_pendulum_with_valueiteration_nd.py
+++ projects/value_iteration/double_pendulum_with_valueiteration_nd.py
@@ -24,33 +24,36 @@ from pyro.planning import discretizer, valueiteration
 # Continuous dynamic system
 sys = pendulum.DoublePendulum()
 
-sys.x_ub = np.array([2,2,2,2])
-sys.x_lb = np.array([-2,-2,-2,-2])
+sys.x_ub = np.array([0.2,0.2,0.2,0.2])
+sys.x_lb = np.array([-0.2,-0.2,-0.2,-0.2])
+
+sys.u_ub = np.array([5,2])
+sys.u_lb = np.array([-5,-2])
 
 # Discrete world
-grid_sys = discretizer.GridDynamicSystem( sys , ( 21, 21, 21, 21) , (3,3) , 0.05)
+grid_sys = discretizer.GridDynamicSystem( sys , ( 11, 11, 11, 11) , (3,3) , 0.05)
 
 # Cost Function
 qcf = sys.cost_function
 
 qcf.xbar = np.array([ 0,0,0,0 ]) # target
-qcf.INF  = 10000
+qcf.INF  = 100
 
 # VI algo
 vi = valueiteration.ValueIteration_ND( grid_sys , qcf )
 
 vi.initialize()
-vi.load_data('aaa')
-vi.compute_steps(12,True)
+#vi.load_data('bbb')
+vi.compute_steps(2,True)
 #vi.load_data()
 vi.assign_interpol_controller()
 vi.plot_policy(0)
 vi.plot_cost2go()
-vi.save_data('aaa')
+vi.save_data('bbb')
 
 
-# Closed loop
-cl_sys = vi.ctl + sys
+#asign controller
+cl_sys = controller.ClosedLoopSystem( sys , vi.ctl )
 
 # Simulation and animation
 cl_sys.x0   = np.array([0.1,0.1,0,0])
