1b6bb642b5667f30daa04b9377b3cd246a9bb318
gabrielcabana21 <cabg2101@usherbrooke.ca>
2021-06-09 16:02:50 -0400

    Fixed deprecated / badly merged file.

--- classes/udes_gmc705/week3/twolinkrobot_kinematic_controller.py
+++ classes/udes_gmc705/week3/twolinkrobot_kinematic_controller.py
@@ -13,31 +13,30 @@ from pyro.dynamic  import manipulator
 ###############################################################################
 
 torque_controlled_robot = manipulator.TwoLinkManipulator()
-speed_controlled_robot  = manipulator.SpeedControlledManipulator( 
-                                             torque_controlled_robot )
+speed_controlled_robot  = manipulator.SpeedControlledManipulator.from_manipulator(torque_controlled_robot)
 
 
 kinematic_controller = robotcontrollers.EndEffectorKinematicController( speed_controlled_robot , 1 )
-<<<<<<< HEAD
-kinematic_controller.rbar = np.array([1,1])
+# <<<<<<< HEAD
+# kinematic_controller.rbar = np.array([1,1])
     
-closed_loop_robot = kinematic_controller + speed_controlled_robot
+# closed_loop_robot = kinematic_controller + speed_controlled_robot
     
-x0        = np.array([-0.5,0.2])
+# x0        = np.array([-0.5,0.2])
     
-closed_loop_robot.plot_animation( x0, 5 )
-closed_loop_robot.sim.plot('xu')
+# closed_loop_robot.plot_animation( x0, 5 )
+# closed_loop_robot.sim.plot('xu')
 
-#closed_loop_robot.rbar = np.array([1.5,0.5])
-#closed_loop_robot.plot_animation( x0, 5 )
-closed_loop_robot.animate_simulation(0.01)
-=======
+# #closed_loop_robot.rbar = np.array([1.5,0.5])
+# #closed_loop_robot.plot_animation( x0, 5 )
+# closed_loop_robot.animate_simulation(0.01)
+# =======
 kinematic_controller.rbar = np.array([0.5,0.5])
     
 closed_loop_robot = kinematic_controller + speed_controlled_robot
     
 x0        = np.array([0.1,0.1])
     
-closed_loop_robot.plot_animation( x0, 5 )
-closed_loop_robot.sim.plot('xu')
->>>>>>> Will
+closed_loop_robot.plot_trajectory('xu')
+closed_loop_robot.animate_simulation(0.01)
+# >>>>>>> Will
